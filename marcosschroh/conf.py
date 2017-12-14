bfe3184df2d23681f046ab4d5c99d2509415eb78
Marcos Schroh <schrohm@gmail.com>
2017-12-14 16:28:49 +0100

    Nikola auto commit.
    Nikola version: 7.8.10

--- marcosschroh/conf.py
+++ marcosschroh/conf.py
@@ -184,8 +184,8 @@ THEME_COLOR = '#5670d4'
 #     )
 
 POSTS = (
-    ("posts/*.rst", "posts", "post.tmpl"),
     ("posts/*.md", "posts", "post.tmpl"),
+    ("posts/*.rst", "posts", "post.tmpl"),
     ("posts/*.txt", "posts", "post.tmpl"),
     ("posts/*.html", "posts", "post.tmpl"),
 )
