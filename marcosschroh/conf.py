b9574c10c9bba58b6b3e9a0c789aa21d4bbcf0f4
Marcos Schroh <schrohm@gmail.com>
2018-01-06 18:39:48 +0100

    Nikola auto commit.
    Nikola version: 7.8.10

--- marcosschroh/conf.py
+++ marcosschroh/conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Marcos Schroh"  # (translatable)
 BLOG_TITLE = "Marcos Schroh"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://marcosschroh.github.io"
+SITE_URL = "https://marcosschroh.github.io/"
 # SITE_URL = 'http://localhost:8000'
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
